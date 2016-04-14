89e338cf17c6c5f79d2b716a357b56a1567840a1
ronen barzel <ronen@barzel.org>
2016-04-14 14:43:42 -0400

    upgrade to latest phantomjs and qunit

--- test/lib/runner-plain.js
+++ test/lib/runner-plain.js
@@ -1,22 +1,82 @@
-var url = phantom.args[0];
-var page = require('webpage').create();
-// Route "console.log()" calls from within the Page context to the main Phantom context (i.e. current "this")
-page.onConsoleMessage = function (msg) {
-    console.log(msg);
-    if (msg.indexOf("Error") == 0) {
+(function () {
+    'use strict';
+
+    var url, page, timeout,
+        args = require('system').args;
+
+    // arg[0]: scriptName, args[1...]: arguments
+    if (args.length < 2) {
+        console.error('Usage:\n  phantomjs [phantom arguments] runner.js [url-of-your-plain-testsuite] [timeout-in-seconds] [page-properties]');
+        exit(1);
+    }
+
+    url = args[1];
+
+    if (args[2] !== undefined) {
+        timeout = parseInt(args[2], 10);
+    }
+
+    page = require('webpage').create();
+
+    if (args[3] !== undefined) {
+        try {
+            var pageProperties = JSON.parse(args[3]);
+
+            if (pageProperties) {
+                for (var prop in pageProperties) {
+                    if (pageProperties.hasOwnProperty(prop)) {
+                        page[prop] = pageProperties[prop];
+                    }
+                }
+            }
+        } catch(e) {
+            console.error('Error parsing "' + args[3] + '": ' + e);
+        }
+    }
+
+    // Route `console.log()` calls from within the Page context to the main Phantom context (i.e. current `this`)
+    page.onConsoleMessage = function (msg) {
+        console.log(msg);
+        if (msg.indexOf("Error") == 0) {
+            phantom.exit(1);
+        }
+        if (msg.indexOf("Done") == 0) {
+            phantom.exit(0);
+        }
+    };
+
+    page.onError = function(msg, trace) {
+        console.log("Error: " + msg);
         phantom.exit(1);
     }
-    if (msg.indexOf("Done") == 0) {
-        phantom.exit(0);
+
+    page.open(url, function (status) {
+        if (status !== 'success') {
+            console.error('Unable to access network: ' + status);
+            exit(1);
+        } else {
+
+            // Set a default timeout value if the user does not provide one
+            if (typeof timeout === 'undefined') {
+                timeout = 5;
+            }
+
+            // Set a timeout on the test running, otherwise tests with async problems will hang forever
+            setTimeout(function () {
+                console.error('The specified timeout of ' + timeout + ' seconds has expired. Aborting...');
+                exit(1);
+            }, timeout * 1000);
+
+            // Do nothing... the callback mechanism will handle everything!
+        }
+    });
+
+    function exit(code) {
+        if (page) {
+            page.close();
+        }
+        setTimeout(function () {
+            phantom.exit(code);
+        }, 0);
     }
-};
-page.onError = function(msg, trace) {
-    console.log("Error: " + msg);
-    phantom.exit(1);
-}
-page.open(url, function (status) {
-    if (status !== "success") {
-        console.log("Unable to load " + url + " : " + status);
-        phantom.exit(1);
-    }
-});
+})();
